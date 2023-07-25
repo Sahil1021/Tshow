cd7d1efcc72e976f59dd644e7f59e493743d52d8
Sahil <sahilrb21@gmail.com>
2023-07-25 17:22:00 +0530

    sahil frontend

--- src/views/CreateShow.vue
+++ src/views/CreateShow.vue
@@ -1,60 +1,67 @@
 <template>
-    <div>
-        <h1>Create a New Show</h1>
-        <form class="container w-25" @submit.prevent="createShow">
-            <label>Theatre ID:</label>
-            <input class="form-control" v-model="theatreId" type="number" required />
-            <br />
-            <label>Show Name:</label>
-            <input class="form-control" v-model="showName" type="text" required />
-            <br />
-            <label>Show Time:</label>
-            <input class="form-control" v-model="showTime" type="text" required />
-            <br />
-            <button class="btn btn-primary" type="submit">Create Show</button>
-        </form>
-        <p v-if="errorMessage">{{ errorMessage }}</p>
-    </div>
+  <div>
+    <h1>Create a New Show</h1>
+    <form class="container w-25" @submit.prevent="createShow">
+      <label>Theatre ID:</label>
+      <input class="form-control" v-model="theatreId" type="number" required />
+      <br />
+      <label>Show Name:</label>
+      <input class="form-control" v-model="showName" type="text" required />
+      <br />
+      <label>Show Date:</label>
+      <input class="form-control" v-model="showDate" type="date" required />
+      <br />
+      <label>Show Time:</label>
+      <input class="form-control" v-model="showTime" type="time" required />
+      <br />
+      <button class="btn btn-primary" type="submit">Create Show</button>
+    </form>
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+  </div>
 </template>
 
 <script>
 import api from "../api";
 export default {
-    data() {
-        return {
-            theatreId: "",
-            showName: "",
-            showTime: "",
-            errorMessage: "",
+  data() {
+    return {
+      theatreId: "",
+      showName: "",
+      showDate: "",
+      showTime: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    async createShow() {
+      try {
+        const showData = {
+          theatre_id: this.theatreId,
+          name: this.showName,
+          date: this.showDate,
+          time: this.showTime,
         };
-    },
-    methods: {
-        async createShow() {
-            try {
-                const showData = {
-                    theatre_id: this.theatreId,
-                    name: this.showName,
-                    time: this.showTime,
-                };
-                const response = await api.post("/shows", showData);
+        const response = await api.post("/shows", showData);
 
-                if (response.status === 201) {
-                    alert("Show created successfully!");
-                    // Clear form inputs after creating a show
-                    this.theatreId = "";
-                    this.showName = "";
-                    this.showTime = "";
-                    this.errorMessage = ""; // Clear any previous error message
-                }
-            } catch (error) {
-                if (error.response && error.response.status === 404) {
-                    this.errorMessage = "Theater not found. Please provide a valid Theater ID.";
-                } else {
-                    this.errorMessage = "An error occurred while creating the show.";
-                }
-                console.error(error);
-            }
-        },
+        if (response.status === 201) {
+          alert("Show created successfully!");
+          // Clear form inputs after creating a show
+          this.theatreId = "";
+          this.showName = "";
+          this.showDate = "";
+          this.showTime = "";
+          this.errorMessage = ""; // Clear any previous error message
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          this.errorMessage =
+            "Theater not found. Please provide a valid Theater ID.";
+        } else {
+          this.errorMessage = "An error occurred while creating the show.";
+        }
+        console.error(error);
+      }
     },
+  },
 };
 </script>
--- src/views/ShowList.vue
+++ src/views/ShowList.vue
@@ -1,75 +1,106 @@
 <template>
-    <div class="container">
-        <h1 class="mb-3">Shows List</h1>
-        <div class="form-group">
-            <label for="theaterInput w-50">Enter Theater Name:</label>
-            <input type="text" class="form-control" id="theaterInput" v-model="theaterInput" />
-            <button class="btn btn-primary mt-2" @click="filterShowsByTheater">
-                Search
-            </button>
-        </div>
-        <div class="row mt-3" v-if="filteredShows.length > 0">
-            <div class="col-12 col-sm-6 col-md-6 mb-4" v-for="show in filteredShows" :key="show.id">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-decoration-underline">Show {{ show.id }}</h5>
-                        <p class="card-text">Show Name: {{ show.name }}</p>
-                        <p class="card-text">Theater Name: {{ show.theatre_name }}</p>
-                        <p class="card-text">Show Time: {{ show.time }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <p v-else>No shows found for the given theater.</p>
-        <router-link to="/shows/create" class="btn btn-primary mb-4">Create New Show</router-link>
+  <div class="container">
+    <h1 class="mb-3">Shows List</h1>
+    <div class="form-group">
+      <label for="dateInput">Select Date:</label>
+      <input
+        type="date"
+        class="form-control"
+        id="dateInput"
+        v-model="selectedDate"
+        @change="filterShows"
+      />
     </div>
+    <div class="form-group">
+      <label for="theaterInput">Enter Theater Name:</label>
+      <input
+        type="text"
+        class="form-control"
+        id="theaterInput"
+        v-model="theaterInput"
+        @input="filterShows"
+      />
+    </div>
+    <div class="row mt-3" v-if="filteredShows.length > 0">
+      <div
+        class="col-12 col-sm-6 col-md-6 mb-4"
+        v-for="show in filteredShows"
+        :key="show.id"
+      >
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-decoration-underline">
+              Show {{ show.id }}
+            </h5>
+            <p class="card-text">Show Name: {{ show.name }}</p>
+            <p class="card-text">Theater Name: {{ show.theatre_name }}</p>
+            <p class="card-text">Show Time: {{ show.time }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p v-else>No shows found for the selected date and theater.</p>
+    <router-link to="/shows/create" class="btn btn-primary mb-4"
+      >Create New Show</router-link
+    >
+  </div>
 </template>
 
 <script>
 import api from "../api";
 
 export default {
-    data() {
-        return {
-            shows: [],
-            filteredShows: [],
-            theaterInput: "", // Input field to get theater name from the user
-        };
+  data() {
+    return {
+      shows: [],
+      selectedDate: "", // Variable to hold the selected date
+      theaterInput: "", // Variable to hold the input theater name
+    };
+  },
+  async created() {
+    try {
+      this.shows = await this.getShows();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  computed: {
+    filteredShows() {
+      // Filter shows based on the selected date and theater name
+      const selectedDate = new Date(this.selectedDate);
+      const keyword = this.theaterInput.trim().toLowerCase();
+
+      if (!selectedDate.getTime() && !keyword) {
+        // Display all shows if no date and theater name are selected
+        return this.shows;
+      }
+
+      return this.shows.filter((show) => {
+        const showDate = new Date(show.date);
+        const theaterName = show.theatre_name.toLowerCase();
+
+        // Use toDateString for date comparison
+        const isDateMatch =
+          !selectedDate.getTime() ||
+          showDate.toDateString() === selectedDate.toDateString();
+
+        // Use includes for partial theater name match
+        const isTheaterMatch = !keyword || theaterName.includes(keyword);
+
+        return isDateMatch && isTheaterMatch;
+      });
     },
-    async created() {
-        try {
-            this.shows = await this.getShows();
-            this.filteredShows = this.shows; // Display all shows initially
-        } catch (error) {
-            console.error(error);
-        }
+  },
+  methods: {
+    async getShows() {
+      try {
+        const response = await api.get("/shows");
+        return response.data; // Return the shows data from the response
+      } catch (error) {
+        console.error(error);
+        return []; // Return an empty array in case of an error
+      }
     },
-    methods: {
-        async getShows() {
-            try {
-                const response = await api.get("/shows");
-                return response.data; // Return the shows data from the response
-            } catch (error) {
-                console.error(error);
-                return []; // Return an empty array in case of an error
-            }
-        },
-        viewTheaterLayout(theaterId) {
-            this.$router.push({ name: "TheaterLayout", params: { theatreId: theaterId } });
-            console.log("View theater layout for theater with ID:", theaterId);
-        },
-        filterShowsByTheater() {
-            // Filter shows based on the theater name input by the user
-            const keyword = this.theaterInput.trim().toLowerCase();
-            if (!keyword) {
-                this.filteredShows = this.shows; // Display all shows if no keyword is provided
-            } else {
-                this.filteredShows = this.shows.filter((show) => {
-                    const theaterName = show.theatre_name.toLowerCase();
-                    return theaterName === keyword; // Use strict equality for exact match
-                });
-            }
-        },
-    },
+  },
 };
 </script>
